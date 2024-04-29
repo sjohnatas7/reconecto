f1086f7092e3b4cff1e1da9bcdd296e0f64e4df4
sjohnatas7 <johnatas@usp.br>
2024-04-29 20:02:28 -0300

    mudancas de next

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
-   images: { unoptimized: true } 
+  images: { unoptimized: true, path: "docs" }
 }
 
 module.exports = nextConfig
